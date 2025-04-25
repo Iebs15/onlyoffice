60c915bbfebe47e070e71da9f876b2346dd34ff6
Iebs15 <ayush.kumar@iebrain.com>
2025-04-25 17:52:32 +0530

    log

--- backend/app.py
+++ backend/app.py
@@ -107,6 +107,13 @@ def onlyoffice_config():
             "url": file_url,
         },
         "documentType": "word",
+        "logo": {
+            "image": "https://ibb.co/Zp31xBZn",
+            "imageDark": "https://ibb.co/Zp31xBZn",
+            "imageLight": "https://ibb.co/Zp31xBZn",
+            "url": "http://20.109.20.242:4000/dashboard",
+            "visible": True,
+        },
         "editorConfig": {
             "user": { "id": "u1", "name": "Test User" },
             "callbackUrl": f"{BACKEND_URL}/save/{filename}",
